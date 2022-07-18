2ab55ad81f619306e83e944e3ed33b237b0bbdb7
VladimirVinageras <vvinageras@gmail.com>
2022-07-19 00:46:10 +0200

    Initial Commit

---
+++ VPN_Ping_Monitoring_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VPN_Ping_Monitoring_App
+//
+//  Created by Vladimir Vinageras on 19.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VPN_Ping_Monitoring_App/VPN_Ping_Monitoring_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPN_Ping_Monitoring_AppApp.swift
+//  VPN_Ping_Monitoring_App
+//
+//  Created by Vladimir Vinageras on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct VPN_Ping_Monitoring_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
